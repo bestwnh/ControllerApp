3016fd71058bc5a7173ad14cffd71b0d58e47390
Galvin <b02437@gmail.com>
2019-10-24 22:27:46 +0800

    Initial Commit

---
+++ ControllerApp/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  ControllerApp
+//
+//  Created by Galvin on 2019/10/24.
+//  Copyright © 2019 GalvinLi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
